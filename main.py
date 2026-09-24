337d89a8734476aadf628ac9ab7a9dfb5c884de5
Lucas Amorim <lcoliveiraamorim50@yahoo.com>
2026-09-24 10:23:54 -0300

    feat: adicionando dicionarios & simplifica

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print ("Hello, World!")
+termos_dificeis = {
+    "pusilânime":"covarde",
+    "rescindir": "cancelar",
+    "pundonor": "orgulho ",
+    "inócuo": "inofensivo",
+    "locador": "dono do imóvel"
+}
+
+def simplifica(texto):
+    palavras = texto.split()
+    
+    resultado = []
+    for palavra in palavras:
+        palavra = palavra.lower()
+        palavra = palavra.strip(",")
+        
+        if palavra in termos_dificeis:
+            palavra = termos_dificeis[palavra]
+        
+        resultado.append(palavra)
+        
+    frase = " ".join(resultado)
+    return frase
+
+print(simplifica("ele vai rescindir o contrato"))
